2eed48bff4928cad3ca0713ee77f9a0879989a5f
EC2 Default User <ec2-user@ip-172-31-21-200.us-east-2.compute.internal>
2019-04-10 15:31:36 +0000

    end of exercise

--- app.js
+++ app.js
@@ -4,30 +4,29 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hi there, welcome to my assignment'));
 
-app.get('/:animal', (req, res) => {
-    let animal = req.params.animal;
-    res.send( () => {
-        if(animal === 'pig'){
-            `The ${animal} says 'oink'`;
-        } else if (animal === 'cow'){
-            `The ${animal} says 'moo'`;
-        } else if(animal === 'dog'){
-           `The ${animal} says 'Woof Woof`; 
-        }else{
-            starRoute;
-        }
-    });
-        
-});
+app.get('/speak/:animal', (req, res) => {
+    let animal = req.params.animal.toLowerCase();
+    let sounds = {
+        dog: 'Woof Woof', 
+        pig: 'Oink', 
+        cow: 'Mooo', 
+        cat: 'I hate you humon', 
+        goldfish: '...'
+    }
+    let sound = sounds[animal];
+    
+    
+    res.send(`The ${animal} says '${sound}'`);
+})
 
-app.get('/:word/:int', (req, res) => {
+app.get('/repeat/:word/:int', (req, res) => {
     let word = req.params.word;
-    let num = req.params.int;
+    let num = Number(req.params.int);
     let repeatable = `${word} `;
     res.send(repeatable.repeat(num));
 }); 
 
 
-const starRoute = app.get('*', (req, res) => 'Sorry, page not found...What are you doing with your life?');
+let starRoute = app.get('*', (req, res) => res.send('Sorry, page not found...What are you doing with your life?'));
 starRoute;
 app.listen(process.env.PORT, process.env.IP, () => console.log('Server is running'));
